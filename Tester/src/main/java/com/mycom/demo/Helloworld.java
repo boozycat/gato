59797eca3a73a47de5bab6c0591e1a1503987fd3
Alice smith <alice@smith.com>
2020-02-17 12:05:45 -0800

    lots of log4j java stuff

--- Tester/src/main/java/com/mycom/demo/Helloworld.java
+++ Tester/src/main/java/com/mycom/demo/Helloworld.java
@@ -1,8 +1,15 @@
 package com.mycom.demo;
 
+import org.apache.log4j.Logger;
+
 public class Helloworld {
 
+    private static Logger LOG = Logger.getLogger("Helloworld");
+
     public static void main(String [] ars){
+        LOG.info("this is an info message");
+        LOG.debug("this is a debug message");
         System.out.println("Helloworld Amdocs Dallas");
+        LOG.error("this is an error message");
     }
 }
